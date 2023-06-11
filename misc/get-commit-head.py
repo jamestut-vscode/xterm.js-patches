6848899de50a153feb20e71a1976adc298b9e3e2
James <aslan.jnn@gmail.com>
2023-06-11 10:15:51 +1000

    Add script to get xterm.js' commit head from NPM

---
+++ misc/get-commit-head.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import argparse
+import urllib.request
+import urllib.error
+import sys
+import json
+
+def printerr(*args, **kwargs):
+   kwargs['file'] = sys.stderr
+   print(*args, **kwargs)
+
+if __name__ == "__main__":
+   URL = "https://registry.npmjs.org/xterm"
+
+   ap = argparse.ArgumentParser(description="This script queries NPM's registry "
+      "to look up the commit hash associated with a given version string.")
+   ap.add_argument("version", help="Version string to look for the commit hash for.")
+   args = ap.parse_args()
+
+   print("Downloading NPM registry for xterm.js ...")
+   try:
+      req = urllib.request.urlopen(URL)
+      d = req.read()
+   except urllib.error.URLError as ex:
+      printerr(f"Error retreiving registry info: {str(ex)}")
+      sys.exit(1)
+
+   d = json.loads(d)
+   d = d.get("versions", {}).get(args.version)
+   if not d:
+      printerr(f"Specified version '{args.version}' is not found.")
+      sys.exit(1)
+
+   git_head = d.get("gitHead")
+   if not git_head:
+      printerr(f"The version '{args.version}' was found, but cannot find the Git head.")
+      sys.exit(1)
+
+   print(git_head)
